6186f636b257ec854b25612833de8793603a503a
Christoph Jerolimov <jerolimov+git+2016@gmail.com>
2016-03-15 01:22:43 +0100

    Prepare new examples

--- examples/ios/examplesTests/examplesTests.m
+++ examples/ios/examplesTests/examplesTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 240
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface examplesTests : XCTestCase
+@interface ExamplesTests : XCTestCase
 
 @end
 
-@implementation examplesTests
+@implementation ExamplesTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
